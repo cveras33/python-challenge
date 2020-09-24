7c4cc82e9f96368c52b6a5c8caf75642f756d807
Chloe Veras <chloe@chloes-air.lan>
2020-09-24 08:53:53 -0400

    added candidates and # of votes to a dictonary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,13 +8,16 @@ print("--------------------------------------------------")
 # Relative path for input file
 # csvPath = os.path.join('..', 'Resources', 'election_data.csv')
 
-csvPath = r"/Users/chloe/Documents/HW_ASSIGNMENTS/python-challenge/PyPoll/Resources/election_data.csv"
+csvPath = r'/Users/chloe/Documents/HW_ASSIGNMENTS/python-challenge/PyPoll/Resources/election_data.csv'
 
-# Variable for total votes 
+# Variable for total votes cast 
 total_votes = 0 
 
 # List to hold vote cast for each candidate 
-candidates = []
+candidates_list = []
+
+# Dictonary to hold each candidate and the number of votes they received
+candidates = {}
 
 with open(csvPath) as csvFileStream:
     # CSV reader specifies delimiter and variable that holds contents
@@ -23,9 +26,26 @@ with open(csvPath) as csvFileStream:
     # Read the header row first
     csv_header = next(csv_reader)
 
-    # Calculating the total number of votes
-    total_votes = sum(1 for row in csv_reader)
+    for row in csv_reader: 
+        candidates_list.append(row[2])
+
+# Getting the total amount of votes cast        
+total_votes = len(candidates_list)
+
+# Adding each candidate to a dictionary and if they've already
+# been added, adding one to their total number of votes
+for candidate in candidates_list:
+    if(candidate in candidates):
+        candidates[candidate] += 1
+    else:
+        candidates[candidate] = 1
 
 # Printing outputs
 print(f"Total Votes: {total_votes}")
 print("--------------------------------------------------")
+
+print(candidates.keys())
+
+#printing 
+#for key, value in candidates.items():
+#    print("%s : %d"%(key, value))
